f05a0780da46af822de7c17b6c5f1f391a7904b2
Jikai Wang <jikai.wang@utdallas.edu>
2024-06-25 17:36:34 -0500

    update hand detector script

--- tools/02_run_hand_detector.py
+++ tools/02_run_hand_detector.py
@@ -17,7 +17,7 @@ MP_CONFIG = {
 }
 
 
-def runner_mp_hand_detector(rgb_images, mp_config, idx=None):
+def runner_mp_hand_detector(rgb_images, mp_config):
     detector = HandDetector(mp_config)
     marks_result = np.full((len(rgb_images), 2, 21, 2), -1, dtype=np.int64)
     for frame_id in range(len(rgb_images)):
@@ -39,7 +39,7 @@ def runner_mp_hand_detector(rgb_images, mp_config, idx=None):
 
     marks_result = marks_result.astype(np.int64)
 
-    return marks_result, idx if idx is not None else marks_result
+    return marks_result
 
 
 def main():
@@ -62,28 +62,26 @@ def main():
     )
 
     mp_handmarks = {serial: None for serial in serials}
+    tqbar = tqdm(total=len(serials), ncols=60, colour="green")
     with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-        tqbar = tqdm(total=len(serials), ncols=60, colour="green")
-        workers = []
-        for serial in serials:
-            workers.append(
-                executor.submit(
-                    runner_mp_hand_detector,
-                    rgb_images=[
-                        loader.get_rgb_image(f_id, serial) for f_id in range(num_frames)
-                    ],
-                    mp_config=MP_CONFIG,
-                    idx=serial,
-                )
-            )
+        futures = {
+            executor.submit(
+                runner_mp_hand_detector,
+                rgb_images=[
+                    loader.get_rgb_image(f_id, serial) for f_id in range(num_frames)
+                ],
+                mp_config=MP_CONFIG,
+            ): serial
+            for serial in serials
+        }
 
-        for worker in concurrent.futures.as_completed(workers):
-            handmarks, serial = worker.result()
-            mp_handmarks[serial] = handmarks
+        for future in concurrent.futures.as_completed(futures):
+            mp_handmarks[futures[future]] = future.result()
             tqbar.update()
             tqbar.refresh()
-        tqbar.close()
-        workers.clear()
+
+        del futures
+    tqbar.close()
 
     logger.info("*** Updating Hand Detection Results with 'mano_sides' ***")
     if mano_sides is not None and len(mano_sides) == 1:
@@ -107,56 +105,53 @@ def main():
                     mp_handmarks[serial][frame_id][0] = -1
 
     logger.info("*** Saving Hand Detection Results ***")
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for serial in serials:
-            tqdm.write(f"  - Saving hand detection results for {serial}")
+    for serial in serials:
+        tqdm.write(f"  - Saving hand detection results for {serial}")
 
-            # Make folder to save hand detection results
-            save_folder = sequence_folder / "processed" / "hand_detection" / serial
-            make_clean_folder(save_folder)
+        # Make folder to save hand detection results
+        save_folder = sequence_folder / "processed" / "hand_detection" / serial
+        make_clean_folder(save_folder)
 
-            # Save hand detection results
-            tqdm.write("    ** Saving Handmarks...")
-            workers = []
-            tqbar = tqdm(total=num_frames, ncols=60, colour="green")
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                for frame_id in range(num_frames):
-                    workers.append(
-                        executor.submit(
-                            np.save,
-                            save_folder / f"handmarks_{frame_id:06d}.npy",
-                            mp_handmarks[serial][frame_id],
-                        )
-                    )
-                for worker in concurrent.futures.as_completed(workers):
-                    worker.result()
-                    tqbar.update()
-                    tqbar.refresh()
-            tqbar.close()
-            workers.clear()
+        # Save hand detection results
+        tqdm.write("    ** Saving Handmarks...")
+        tqbar = tqdm(total=num_frames, ncols=60, colour="green")
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(
+                    np.save,
+                    save_folder / f"handmarks_{frame_id:06d}.npy",
+                    mp_handmarks[serial][frame_id],
+                ): frame_id
+                for frame_id in range(num_frames)
+            }
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
+                tqbar.update()
+                tqbar.refresh()
+            del futures
+        tqbar.close()
 
-            tqdm.write(f"    ** Saving vis images...")
-            workers = []
-            tqbar = tqdm(total=num_frames, ncols=60, colour="green")
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = {
-                    executor.submit(
-                        draw_debug_image,
-                        loader.get_rgb_image(frame_id, serial),
-                        hand_marks=mp_handmarks[serial][frame_id],
-                        draw_boxes=True,
-                        draw_hand_sides=True,
-                        save_path=save_folder / f"vis_{frame_id:06d}.png",
-                        return_image=False,
-                    ): frame_id
-                    for frame_id in range(num_frames)
-                }
-                for future in concurrent.futures.as_completed(futures):
-                    future.result()
-                    tqbar.update()
-                    tqbar.refresh()
-            tqbar.close()
-            workers.clear()
+        tqdm.write(f"    ** Saving vis images...")
+        tqbar = tqdm(total=num_frames, ncols=60, colour="green")
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(
+                    draw_debug_image,
+                    loader.get_rgb_image(frame_id, serial),
+                    hand_marks=mp_handmarks[serial][frame_id],
+                    draw_boxes=True,
+                    draw_hand_sides=True,
+                    save_path=save_folder / f"vis_{frame_id:06d}.png",
+                    return_image=False,
+                ): frame_id
+                for frame_id in range(num_frames)
+            }
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
+                tqbar.update()
+                tqbar.refresh()
+            del futures
+        tqbar.close()
 
     logger.info(">>>>>>>>>> Hand Detection Completed <<<<<<<<<<")
 
